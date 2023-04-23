5bc99906e07414f46d3951c068f4cdb3b6ce9cec
Suneel Saroj <57642610+SuneelSaroj@users.noreply.github.com>
2023-04-23 17:59:36 +0530

    vercel setup done

--- courses/settings.py
+++ courses/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-35e%4+h*pc_=s_r#w7%!@lr@p5_eiipeo7bh39#j=4wcc0r$8x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -119,8 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATICFILES_DIR = ['static']
+# STATICFILES_DIR = ['static']
+STATICFILES_DIR = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static'),
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
